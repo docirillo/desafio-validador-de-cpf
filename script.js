b33a7f67057f81270cddde9994827c4584143fee
Daniel Cirillo <daniel.orlandi@outlook.com>
2022-05-14 10:40:10 -0300

    updates

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log('JavaScript carregado');
+
+function validator() {
+  console.log('Iniciando a validação CPF');
+
+  function validateCPF(cpf) {
+      
+  }
+
+  let cpf = document.getElementById('cpf_digitado').value;
+
+  let resultValidator = validateCPF(cpf);
+
+  if(resultValidator == true) {
+    
+  }
+  
+  console.log(cpf);
+}
